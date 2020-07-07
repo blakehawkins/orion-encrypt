6b5c966b08b47640e9b6bb20d72f764addbd0fb6
blakehawkins <blake.hawkins.11@gmail.com>
2020-07-07 21:51:07 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+Generate a base64-encoded, XChaCha20Poly1305-encrypted blob from a password and an input.
+
+```bash
+$ orion-encrypt "pass phrase" < plaintext > ciphertext
+```
---
+++ src/main.rs
@@ -0,0 +1,24 @@
+use std::io::Read;
+
+#[paw::main]
+fn main(args: paw::Args) -> std::io::Result<()> {
+    let args = args.collect::<Vec<_>>();
+    if args.len() != 2 || args.iter().next().unwrap() == "--help" {
+        println!("Usage: orion-encrypt \"pass phrase\" < plaintext > ciphertext");
+        std::process::exit(1);
+    }
+
+    let pass_phrase = format!("{:<32}", args.into_iter().nth(1).unwrap());
+
+    let key = orion::aead::SecretKey::from_slice(&pass_phrase.as_bytes()[0..32])
+        .expect("failed to build secret key from pass phrase");
+
+    let mut buf = String::new();
+    std::io::stdin().lock().read_to_string(&mut buf)?;
+
+    let ciphertext = orion::aead::seal(&key, buf.as_bytes()).expect("failed to seal");
+
+    println!("{}", base64::encode(ciphertext));
+
+    Ok(())
+}
